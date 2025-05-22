63d93ef29802d8afabdc518fe811e69ea50ec63d
IlyasHAMDAOUI <muhammadilyas.hamdaoui@latoile.em-lyon.com>
2025-05-22 19:13:13 +0200

    en cours d'amelioration
    a

--- script.js
+++ script.js
@@ -75,7 +75,39 @@ let competence = {
   },
 };
 
-fetch("test.json")
+fetch("hardskill.json")
 .then((response) => response.json())
-.then(data)
-console.log(data);
+.then(data => {
+
+    console.log(data);
+    data.forEach(element => {
+        console.log(element);
+        const div = document.createElement("div")
+        div.classList.add("card")
+    });
+});
+
+fetch("webDesign.json")
+.then((response) => response.json())
+.then(data => {
+
+    console.log(data);
+    data.forEach(element => {
+        console.log(element);
+        const div = document.createElement("div")
+        div.classList.add("card")
+    });
+});
+
+fetch("wordPress.json")
+.then((response) => response.json())
+.then(data => {
+
+    console.log(data);
+    data.forEach(element => {
+        console.log(element);
+        const div = document.createElement("div")
+        div.classList.add("card")
+    });
+});
+    
